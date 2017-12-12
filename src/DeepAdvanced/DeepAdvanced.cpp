cf30b1a4cfe125152ef226bb3c70bd4a9de6c144
jiahsuanlo <jiahsuanlo@yahoo.com>
2017-12-11 23:21:43 -0500

    update cmake lists update deep advanced example add vehicle detector project

--- src/DeepAdvanced/DeepAdvanced.cpp
+++ src/DeepAdvanced/DeepAdvanced.cpp
@@ -1,117 +1,92 @@
-// DeepAdvanced.cpp : Defines the entry point for the console application.
-//
+// dlib dnn practice
 
 #include <dlib/dnn.h>
 #include <iostream>
 #include <dlib/data_io.h>
 
-// type def block layer
-// Nx3x3-conv  -> BN -> relu -> Nx3x3-conv -> BN
-template <
-	int N,
-	template <typename> class BN,
+using namespace dlib; // conv layer
+
+// define a resnet block layer
+template<int num_filter,
+	template<typename> class BN,
 	int stride,
-	typename SUBNET
->
-using block = BN<dlib::con<N, 3, 3, stride, stride, dlib::relu<BN<dlib::con<N, 3, 3, stride, stride, SUBNET>>>>>;
+	typename SUBNET>
+	using block = BN < con<num_filter, 3, 3, 1, 1,
+	relu<BN<con<num_filter, 3, 3, stride, stride,
+	SUBNET>>>>>;
 
-// define skip/residual layer alias
-/* 
-	  subnet input
-	   |      |
-       \    block
-	    \   /
-	     add
-		  |
-		output
-*/
+// define a resnet residual block
 template<
-	template<int, template<typename>class, int, typename> class block,
-	int N,
+	template<int, template<typename> class, int, typename> class block,
+	int num_filter,
 	template<typename> class BN,
-	typename SUBNET
->
-using residual = dlib::add_prev1<block<N, BN, 1, dlib::tag1<SUBNET>>>; // stride 1
+	typename SUBNET>
+	using residual = add_prev1<block<num_filter, BN, 1, tag1<SUBNET>>>;
 
-// define residual down-sampling layer
-// residual_down creates a network structure like this:
-/*
-	input from SUBNET
-	 /     \
-	/       \
- block     downsample(using avg_pool)
-	\       /
-     \     /
-	add tensors (using add_prev2 which adds the output of tag2 with avg_pool's output)
-	    |
-	  output
-*/
+// define downsampling (stride2) residual block
 template<
-	template <int, template<typename>class, int, typename> class block,
-	int N,
+	template<int, template<typename> class, int, typename> class block,
+	int num_filter,
 	template<typename> class BN,
-	typename SUBNET
->
-using residual_down = dlib::add_prev2<dlib::avg_pool<2, 2, 2, 2,
-	dlib::skip1<dlib::tag2<block<N, BN, 2, dlib::tag1<SUBNET>>>>>>;
+	typename SUBNET>
+	using residual_down = add_prev2 < avg_pool<2, 2, 2, 2,
+	skip1<tag2<block<num_filter, BN, 2, tag1<SUBNET>>>>>>;
+
+// now define 4 different residual blocks
+template <typename SUBNET> using res = relu<residual<block, 8, bn_con, SUBNET>>;
+template <typename SUBNET> using res_a = relu < residual<block, 8, affine, SUBNET>>;
+template <typename  SUBNET> using res_down = relu<residual_down<block, 8, bn_con, SUBNET>>;
+template <typename SUBNET> using resa_down = relu<residual_down<block, 8, affine, SUBNET>>;
+
+// building the net type
+const unsigned long num_classes = 10;
+using net_type = loss_multiclass_log < fc<num_classes,
+	avg_pool_everything<res<res<res<res_down<
+	repeat<9, res,
+	res_down<res<
+	input<matrix<unsigned char>>>>>>>>>>>>;
 
 
-// define 4 different residual layer with and without down-sampling, 
-// with and without batch-normalization. Affine version will be used
-// when testing the network
-template <typename SUBNET> using res = dlib::relu<residual<block, 8, dlib::bn_con, SUBNET>>;
-template <typename SUBNET> using ares = dlib::relu<residual<block, 8, dlib::affine, SUBNET>>;
-template <typename SUBNET> using res_down = dlib::relu<residual_down<block, 8, dlib::bn_con, SUBNET>>;
-template <typename SUBNET> using ares_down = dlib::relu<residual_down<block, 8, dlib::affine, SUBNET>>;
-
-// define the real network now
-const unsigned long number_of_classes = 10;
-using net_type = dlib::loss_multiclass_log < dlib::fc < number_of_classes,
-		dlib::avg_pool_everything < res < res < res < res_down <
-			dlib::repeat <9, res,
-				res_down < res<dlib::input<dlib::matrix<unsigned char>>
-	>>>>>>>>>>;
-
-// using parametric relu to design a network
-template <typename SUBNET>
-using pres = dlib::prelu < dlib::add_prev1<dlib::bn_con<dlib::con<8, 3, 3, 1, 1,
-	dlib::prelu<dlib::bn_con<dlib::con<8, 3, 3, 1, 1, dlib::tag1<SUBNET>  >>>>>>>;
-
-// need mnist images to run
-
-int main()
+int main(int argc, char**argv)
 {
 	// load data
-	std::vector<dlib::matrix<unsigned char>> train_images;
-	std::vector<unsigned long> train_labels;
-	std::vector<dlib::matrix<unsigned char>> test_images;
-	std::vector<unsigned long> test_labels;
-	dlib::load_mnist_dataset("../data/mnistimages", train_images,
-		train_labels, test_images, test_labels);
+	std::vector<matrix<unsigned char>> training_images;
+	std::vector<unsigned long> training_labels;
+	std::vector<matrix<unsigned char>> testing_images;
+	std::vector<unsigned long> testing_labels;
+	load_mnist_dataset("../data/mnistImages", training_images, training_labels, testing_images, testing_labels);
 
-	// set cudnn to use smaller ram required algorithm
-	dlib::set_dnn_prefer_smallest_algorithms();
+	// use smaller ram 
+	set_dnn_prefer_smallest_algorithms();
 
-	// create a network
+	// create a net
 	net_type net;
 
-	// practice: replace relu layers with prelu layers
-	using net_type2 = dlib::loss_multiclass_log<dlib::fc<number_of_classes, 
-		dlib::avg_pool_everything<
-			pres<res<res<res_down<
-				dlib::tag4<dlib::repeat<9,pres,
-					res_down< 
-						res<dlib::input<dlib::matrix<unsigned char>> >>>>>>>>>>>;
-	// create a parametric network
-	net_type2 pnet(dlib::prelu_(0.2), dlib::prelu_(0.25),
-		dlib::repeat_group(dlib::prelu_(0.3), dlib::prelu_(0.4)));
+	// print layer info
+	std::cout << "net has " << net.num_layers << " layers\n";
+	//std::cout << net << "\n";
+	
+	// get output for layer 3 ==>  layer<3>(net).get_output()
+	
+	// now set trainer
+	dnn_trainer<net_type, adam> trainer(net, adam(0.0005, 0.9, 0.999));
+	trainer.be_verbose();
+	trainer.set_iterations_without_progress_threshold(2000);
+	trainer.set_learning_rate_shrink_factor(0.1);
+	trainer.set_learning_rate(0.001);
+	trainer.set_synchronization_file("./DeepAdvanced.dir/mnist_res_sync",
+		std::chrono::seconds(100));
 
-	// print the detail of the pnet
-	std::cout << "The pnet has: " << pnet.num_layers << " layers in it:\n";
-	std::cout << pnet << std::endl;
+	// set mini batch
+	std::vector<matrix<unsigned char>> mini_batch_samples;
+	std::vector<unsigned long> mini_batch_labels;
 
+	while (trainer.get_learning_rate >= 1e-6)
+	{
 
+	}
+	
 	std::system("pause");
-    return 0;
+	return 0;
 }
 
